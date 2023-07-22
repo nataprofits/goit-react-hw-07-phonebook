e3ba0968fe82ea11631a6961f80b2f46fe5b0391
Tkachenko Natalia <nataprofits@gmail.com>
2023-07-22 16:39:01 +0100

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,30 @@
 
-import ContactForm from 'components/ContactForm/ContactForm';
+import { Title } from './App.styled';
+import ContactForm from '../ContactForm/ContactForm';
 import { ContactList } from '../ContactList/ContactList';
 import { Filter } from '../Filter/Filter';
-import {Container, Title, Div} from './App.styled';
+import { Layout } from '../Layout';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from 'redux/operations';
+import { selectError, selectIsLoading } from 'redux/contactsSlice';
 
-export default function App () {
+export default function App() {
+  const dispatch = useDispatch();
+  const error = useSelector(selectError);
+  const isLoading = useSelector(selectIsLoading);
 
+  useEffect(() => {
+    dispatch(fetchContacts())
+  },[dispatch])
   return (
-    <Container>
-      <Div>
+    <Layout>
       <Title>Phonebook</Title>
       <ContactForm />
-      </Div>
-      <Div>
       <Title>Contacts</Title>
       <Filter />
+      {isLoading && !error && <b>Request in progress...</b>}
       <ContactList />
-    </Div>
-    </Container>
+    </Layout>
   );
-};
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
-import App from 'components/App/App';
+import { store } from 'redux/store';
+import App from 'components/App';
 import './index.css';
-import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-    <App />
-      </PersistGate>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+     <App />
+    </Provider>
+  </React.StrictMode>
 );
